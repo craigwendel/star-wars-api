cf525b2dec5c7cadff074d694b4505eaa3f690ee
Craig Wendel <wendelcraig@gmail.com>
2017-07-27 10:33:26 -0500

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import './styles/App.css'
+import registerServiceWorker from './registerServiceWorker'
+
+// IMPORT REACT ROUTER <<<<<<<<<<<<<<<<<<<<<<
+import {BrowserRouter, Route, Switch, NavLink, Link} from 'react-router-dom'
+// IMPORT COMPONENTS <<<<<<<<<<<<<<<<<<<<<<
+import Explorer from './components/Explorer'
+import BaseLayout from './components/Layout'
+import Films from './components/Films'
+import People from './components/People'
+import Starships from './components/Starships'
+
+ReactDOM.render(
+
+<BrowserRouter>
+  <BaseLayout>
+    <Switch>
+      <Route exact path='/' component={Explorer} />
+      <Route path='/characters' component={People} />
+      <Route path='/starships' component={Starships} />
+      <Route path='/films' component={Films} />
+    </Switch>
+  </BaseLayout>
+</BrowserRouter>
+
+, document.getElementById('root'))
+registerServiceWorker()
